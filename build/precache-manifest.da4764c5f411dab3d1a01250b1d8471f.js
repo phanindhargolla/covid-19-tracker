f8bc4f912b9ec9309c73d374b0c7837bedb3e2cc
phanindhargolla <phani_golla@yahoo.com>
2020-11-10 08:29:07 -0500

    Add files via upload

---
+++ build/precache-manifest.da4764c5f411dab3d1a01250b1d8471f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4347ef7406ada3fa6a11644b0b7f9282",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c14f65fd43452c8231a",
+    "url": "/static/css/main.b6a95d0c.chunk.css"
+  },
+  {
+    "revision": "ae00cca395db9becdb62",
+    "url": "/static/js/2.33f83b42.chunk.js"
+  },
+  {
+    "revision": "a52d103a09b0299b4cd97cfd81169642",
+    "url": "/static/js/2.33f83b42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c14f65fd43452c8231a",
+    "url": "/static/js/main.abe847e3.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
